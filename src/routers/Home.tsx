4e3a5ee55efee5aae3ca403e177f295c7718daaf
Khoale38 <kevinle38@gmail.com>
2022-10-05 19:23:59 +0700

    learned route with param

--- src/routers/Home.tsx
+++ src/routers/Home.tsx
@@ -1,25 +1,59 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../styles/Home.scss";
+import { useRef, useState } from "react";
+import ProfileService from "../service/ProfileService";
+import Profile from "../models/Profile";
+
 const Main: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+  let form:Profile;
+  async function axiosTest() {
+    // create a promise for the axios request
+
+    const promise = ProfileService.find(inputRef.current!.value);
+    // using .then, create a new promise which extracts the data
+    const dataPromise: Profile = await promise.then((response) => response);
+    form=dataPromise
+
+  }
+  function onClickHandle(e: any) {
+    axiosTest();
+
+    navigate("/record", {
+      state: {
+        date:form.date,
+        message:form.message,
+        name:form.name,
+        image:form.image,
+      },
+    });
+  }
+
   return (
     <div className="Home-bg d-flex">
       <div className="card custom-card mx-auto">
         <div className="mb-3 ">
-          <div className="form-label  text-align">
-            Input Record Code
-          </div>
+          <div className="form-label  text-align">Input Record Code</div>
           <input
+            ref={inputRef}
             type="email"
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
           />
         </div>
-          <button className="btn btn-grad mb-3" type="button">
-            Submit
-          </button>
-          <a href="/gif" className="mx-auto text-color">I am feeling lucky today - Get a random Gif </a>
-       
+        <button
+          className="btn btn-grad mb-3"
+          type="button"
+          onClick={onClickHandle}
+        >
+          Submit
+        </button>
+        <a href="/gif" className="mx-auto text-color">
+          I am feeling lucky today - Get a random Gif{" "}
+        </a>
       </div>
     </div>
   );
--- src/routers/Record.tsx
+++ src/routers/Record.tsx
@@ -3,26 +3,11 @@ import "../styles/Record.scss";
 import ProfileService from "../service/ProfileService";
 import { useState, useEffect } from "react";
 import Profile from "../models/Profile";
+import { useLocation } from "react-router-dom";
 const Record = () => {
-  const [form, setForm] = useState<Profile>({
-    name: "",
-    mail: "",
-    phone: "",
-    date: "", //moment(Date.now()).format('YYYY-MM-DD')
-    message: "",
-    image: "",
-  });
-  async function axiosTest() {
-    // create a promise for the axios request
-    const promise = ProfileService.find("19330049-f0ee-4ade-8bfa-038c71906830");
-    // using .then, create a new promise which extracts the data
-    const dataPromise = await promise.then((response) => response);
-    setForm(dataPromise);
-  }
-  useEffect(() => {
-    axiosTest();
-  }, []);
-
+  let form: any;
+  const params = useLocation();
+  form = params.state;
   return (
     <div className="Record-bg d-flex">
       <div className="card record-card custom-card  mx-auto">
@@ -31,7 +16,6 @@ const Record = () => {
             On <b className="yellow-voyage">{form.date?.slice(0, 10)} </b>,{" "}
             <b className="yellow-voyage">{form.name}</b> send this message :
             <div className="mt-3  yellow-voyage">
-            
               "<b className="message-text">{form.message}</b>"
             </div>
           </span>
